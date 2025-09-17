7daa43c00dc196253c9fffb789cc51892fc44e01
Fire3Duck <pauavellanedalopez@gmail.com>
2025-09-17 12:40:36 +0200

    GroundSensor bien puesto

--- Assets/Scripts/Player_Controler.cs
+++ Assets/Scripts/Player_Controler.cs
@@ -4,16 +4,24 @@ using UnityEngine.InputSystem;
 public class Player_Controler : MonoBehaviour
 {
     private Rigidbody2D _rigidBody;
-
+    //private Ground_Sensor _groundSensor;
     private InputAction _moveAction;
     private Vector2 _moveInput;
     private InputAction _jumpAction;
     private InputAction _attackAction;
+
     [SerializeField] private float _playerVelocity = 5;
+    [SerializeField] private float _jumpHeight = 2;
+
+    [SerializeField] private Transform _sensorPosition;
+    [SerializeField] private Vector2 _sensorSize = new Vector2(0.5f, 0.5f);
+
 
     void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>(); //Para mover el personaje.
+        //_groundSensor = GetComponentInChildren<Ground_Sensor>();
+
         _moveAction = InputSystem.actions["Move"]; //Si ponemos el .FindAction usaremos los parentesis.
         _jumpAction = InputSystem.actions["Jump"];
         _attackAction = InputSystem.actions["Attack"];
@@ -32,8 +40,8 @@ public class Player_Controler : MonoBehaviour
         Debug.Log(_moveInput);
 
         //transform.position = transform.position + new Vector3(_moveInput.x, 0, 0) * _playerVelocity * Time.deltaTime;
-        
-        if (_jumpAction.WasPressedThisFrame())
+
+        if (_jumpAction.WasPressedThisFrame() && IsGrounded())
         {
             Jump();
         }
@@ -41,12 +49,32 @@ public class Player_Controler : MonoBehaviour
 
     void FixedUpdate()
     {
-        _rigidBody.linearVelocity = new Vector2(_moveInput.x, 0) * _playerVelocity;
+        _rigidBody.linearVelocity = new Vector2(_moveInput.x * _playerVelocity, _rigidBody.linearVelocityY);
     }
 
     void Jump()
     {
-        
+        _rigidBody.AddForce(transform.up * Mathf.Sqrt(_jumpHeight * -2 * Physics2D.gravity.y), ForceMode2D.Impulse);
         Debug.Log("Salto");
+    }
+
+    bool IsGrounded()
+    {
+        Collider2D[] ground = Physics2D.OverlapBoxAll(_sensorPosition.position, _sensorSize, 0);
+        foreach (Collider2D item in ground)
+        {
+            if (item.gameObject.layer == 3)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_sensorPosition.position, _sensorSize);        
     }
 }
